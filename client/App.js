60f221b71bf2d962cf7bdfb0458f4b74b9d24000
sadiejay <19538219+sadiejay@users.noreply.github.com>
2022-02-24 22:16:06 -0600

    Adds coordinate system logic

--- client/App.js
+++ client/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import WallEImg from './images/wall-e.png';
+import Coordinate from './Coordinate-system'
 
 function App() {
 	return (
 		<div className='App' style={{ textAlign: 'center' }}>
 			<img src={WallEImg} height="200" alt="wall-e robot"/>
 			<h1>MARS ROVER</h1>
+			{/* <Coordinate /> */}
 		</div>
 	);
 }
---
+++ client/Coordinate-system.js
@@ -0,0 +1,5 @@
+// https://vectorjs.org/examples/cartesian-coordinate-system/
+
+// I would create a function that was the logic for the coordinate system
+
+// export default Coordinates;
